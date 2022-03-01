844141d36f66308084e29ad2d291ef8d633c0861
Huu Nguyen <thnguyennt1998@gmail.com>
2022-03-01 20:51:49 +0700

    first commit

---
+++ src/mainday1/HelloWorld.java
@@ -0,0 +1,14 @@
+package mainday1;
+
+public class HelloWorld {
+	// can chinh dong--> ctrl + shift + F
+	// xoa 1 dong --> ctrl + D
+	// tu dong import, xoa import khong can thiet --> ctrl + shift + O
+	// save all files --> ctrl + shift + S
+
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World.");
+	}
+
+}
