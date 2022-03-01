e5c0d8ca1ff6f82ea64d5a518ba7e4bcac52ca09
TranHuuNguyen1998 <thnguyennt1998@gmail.com>
2022-03-01 21:13:49 +0700

    01/03/2022: Print my name

--- src/mainday1/HelloWorld.java
+++ src/mainday1/HelloWorld.java
@@ -6,9 +6,20 @@ public class HelloWorld {
 	// tu dong import, xoa import khong can thiet --> ctrl + shift + O
 	// save all files --> ctrl + shift + S
 
-public static void main(String[] args) {
+	// git init: khoi tao
+	// git add README.md: add all files muon push len github
+	// git commit -m "first commit": Dat tieu de cho files muon push
+	// git branch -M main: tao branch main
+	// git remote add origin https://github.com/TranHuuNguyen1998/HelloWorld1.git
+	// git push -u origin main
+	// git config --global user.email "you@example.com": Nhap mail
+	// git config --global user.name "Your Name": nhap ten
+
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World.");
+		
+		System.out.println("Hello World.");
+		System.out.println("My name: Tran Nguyen");
 	}
 
 }
